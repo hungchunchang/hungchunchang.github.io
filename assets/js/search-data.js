71aa8a92aa153d650d7c706e4c1c97dbc511f750
hungchunchang <117837172+hungchunchang@users.noreply.github.com>
2026-01-13 03:24:06 +0000

    Deploying to gh-pages from @ hungchunchang/hungchunchang.github.io@04bf8754ac1e28e6279e79438a9234c0bac370df 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Collections of projects from lab and courses",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
